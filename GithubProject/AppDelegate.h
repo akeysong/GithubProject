ea78c79046aef72ca44c45eadd307435822cfad8
democ <akeysong@gmail.com>
2016-07-07 14:45:01 +0800

    初始Github
    github

---
+++ GithubProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubProject
+//
+//  Created by apple on 16/7/7.
+//  Copyright © 2016年 zhanyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubProject
+//
+//  Created by apple on 16/7/7.
+//  Copyright © 2016年 zhanyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
